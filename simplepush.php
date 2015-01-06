45fc4f02d6c5b2688c4f36c75c77be13a6480a34
高考这件小事 <pupboss.vip@gmail.com>
2015-01-06 13:17:17 +0800

    second commit

---
+++ simplepush.php
@@ -0,0 +1,57 @@
+<?php
+
+// Put your device token here (without spaces):
+// 放置你的 device token:
+$deviceToken = '****';
+
+// Put your private key's passphrase here:
+// 放置你的私钥密码:
+$passphrase = 'xxxx';
+
+// Put your alert message here:
+// 放置你的提示信息:
+$message = '杰哥是天才！!';
+
+// Put your category id here:
+// 放置你的分类id:
+$category = 'alert';
+////////////////////////////////////////////////////////////////////////////////
+
+$ctx = stream_context_create();
+stream_context_set_option($ctx, 'ssl', 'local_cert', 'ck.pem');
+stream_context_set_option($ctx, 'ssl', 'passphrase', $passphrase);
+
+// Open a connection to the APNS server
+$fp = stream_socket_client(
+	'ssl://gateway.sandbox.push.apple.com:2195', $err,
+	$errstr, 60, STREAM_CLIENT_CONNECT|STREAM_CLIENT_PERSISTENT, $ctx);
+
+if (!$fp)
+	exit("Failed to connect: $err $errstr" . PHP_EOL);
+
+echo 'Connected to APNS' . PHP_EOL;
+
+// Create the payload body
+$body['aps'] = array(
+	'alert' => $message,
+	'badge' => 1,
+	'category' => $category,
+	'sound' => 'default'
+	);
+
+// Encode the payload as JSON
+$payload = json_encode($body);
+
+// Build the binary notification
+$msg = chr(0) . pack('n', 32) . pack('H*', $deviceToken) . pack('n', strlen($payload)) . $payload;
+
+// Send it to the server
+$result = fwrite($fp, $msg, strlen($msg));
+
+if (!$result)
+	echo 'Message not delivered' . PHP_EOL;
+else
+	echo 'Message successfully delivered' . PHP_EOL;
+
+// Close the connection to the server
+fclose($fp);
